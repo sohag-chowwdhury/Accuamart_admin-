0529900f6b58f98dd14effb3424ebe727905eded
Md. Sohag Chowdhury <49018206+sohag-chowwdhury@users.noreply.github.com>
2020-12-29 23:16:10 +0600

    Update main.js

--- src/main.js
+++ src/main.js
@@ -42,14 +42,7 @@ new Vue({
   render: h => h(App),
   created () {
     firebase.initializeApp({
-      apiKey: "AIzaSyAzk17Arfjgsrk6Nu3pd06y-FHpZO7ao-U",
-      authDomain: "fresh-by-acumartt.firebaseapp.com",
-      databaseURL: "https://fresh-by-acumartt.firebaseio.com",
-      projectId: "fresh-by-acumartt",
-      storageBucket: "fresh-by-acumartt.appspot.com",
-      messagingSenderId: "175626714869",
-      appId: "1:175626714869:web:188880cf54ff8a398a0fbc",
-      measurementId: "G-7N880Q9VCE" 
+   
     })
     firebase.auth().onAuthStateChanged((user)=>{
       if(user) {
